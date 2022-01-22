344620cdb529140c0c9ea8a049fea66dc6fe9063
Raptoria <tina.lacey@gmail.com>
2022-01-22 16:17:59 -0500

    rename main to home

--- packages/webapp/src/pages/dag.tsx
+++ packages/webapp/src/pages/dag.tsx
@@ -2,36 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { Header, Layout, PageContainer } from '@myscope/components';
 import { FlowChart } from '@myscope/visualizations';
 import axios from 'axios';
-import { getNavButtons } from '../modules/main/NavButtons';
+import { getNavButtons } from '../modules/home/NavButtons';
 
 const DagViewer: React.FC = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios(
-                'https://www.ag-grid.com/example-assets/olympic-winners.json'
-            );
-            setData(result.data);
-        };
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios(
+				'https://www.ag-grid.com/example-assets/olympic-winners.json'
+			);
+			setData(result.data);
+		};
 
-        fetchData();
-    }, []);
-    return (
-        <Layout
-            title='Webapp'
-            subTitle='Dag Viewer'
-            extra={getNavButtons('dag')}
-        >
-            <PageContainer>
-                <Header size='small'>A Flow Chart</Header>
-                <FlowChart />
+		fetchData();
+	}, []);
+	return (
+		<Layout
+			title='Webapp'
+			subTitle='Dag Viewer'
+			extra={getNavButtons('dag')}
+		>
+			<PageContainer>
+				<Header size='small'>A Flow Chart</Header>
+				<FlowChart />
 
-                <Header size='small'>Some JSON Data</Header>
-                {data.slice(0, 10).map((row, i) => (
-                    <div key={`row.athlete ${i}`}>{row.athlete}</div>
-                ))}
-            </PageContainer>
-        </Layout>
-    );
+				<Header size='small'>Some JSON Data</Header>
+				{data.slice(0, 10).map((row, i) => (
+					<div key={`row.athlete ${i}`}>{row.athlete}</div>
+				))}
+			</PageContainer>
+		</Layout>
+	);
 };
 export default DagViewer;
--- packages/webapp/src/pages/index.tsx
+++ packages/webapp/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Layout, PageContainer } from '@myscope/components';
-import { getNavButtons } from '../modules/main/NavButtons';
+import { getNavButtons } from '../modules/home/NavButtons';
 
 const Index: React.FC = () => {
-    return (
-        <Layout title='Webapp' extra={getNavButtons('home')}>
-            <PageContainer>Some content here</PageContainer>
-        </Layout >
-    );
+	return (
+		<Layout title='Webapp' extra={getNavButtons('home')}>
+			<PageContainer>Some content here</PageContainer>
+		</Layout>
+	);
 };
 export default Index;
